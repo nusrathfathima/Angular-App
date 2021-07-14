15403382513a66c3d58fffa4f915a187557afd78
nusrathfathima <nusrathfathima567@gmail.com>
2021-07-14 19:57:26 -0400

    Assignment-2 Data Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
-  styles: [`
-    h3 {
-      color: dodgerblue;
-    }
-  `]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  userName = "";
+
+  onUpdateName(event: Event) {
+    this.userName= (<HTMLInputElement>event.target).value;
+  }
+
+  onClicking() {
+    this.userName = "";
+  }
 }
