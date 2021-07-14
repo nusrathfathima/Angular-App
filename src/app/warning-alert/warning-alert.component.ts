51494ff9d21e9ce4665f5a9ad89290cd2804aadb
nusrathfathima <nusrathfathima567@gmail.com>
2021-07-14 18:20:31 -0400

    Assignment-1

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  templateUrl: './warning-alert.component.html',
+  styles: [`p { color: red; padding: 10px}`]
+})
+
+export class WarningAlertComponent {
+
+}
