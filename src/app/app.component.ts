14a1854d6338547f7da008f33cc0ecff60a2627d
nusrathfathima <nusrathfathima567@gmail.com>
2021-09-14 21:03:48 -0400

    Splitting Apps into Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,25 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: [
-    `
-      .blueColor {
-        color: white;
-      }
-    `,
-  ],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  buttonClicking = 'Secret Password = hello';
-  variable = false;
-  clicks = [];
-  timesClicked = 0;
+  serverElements = [{type: 'server', name: 'Testserver', content: 'Just a test!'}];
 
-  constructor() {}
 
-  onClickButton() {
-    this.variable = !this.variable;
-    this.timesClicked = this.clicks.push(this.clicks.length + 1);
-    this.buttonClicking = 'Secret Password = tuna';
-  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
+import { HelloComponent } from './hello/hello.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CockpitComponent,
+    ServerElementComponent,
+    HelloComponent
   ],
   imports: [
     BrowserModule,
